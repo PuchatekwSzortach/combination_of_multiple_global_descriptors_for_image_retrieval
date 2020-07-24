2945f57e59073521344f21f2ace4baf0feedd1aa
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2020-07-24 21:34:11 +0900

    Added visualize.visualize-tf-dataset invoke task
    Shows how to create, and work with, tf.Dataset instances for our data

--- net/invoke/visualize.py
+++ net/invoke/visualize.py
@@ -50,6 +50,58 @@ def visualize_data(_context, config_path):
 
 
 @invoke.task
+def visualize_tf_dataset(_context, config_path):
+    """
+    Visualize data based on tensorflow dataset
+
+    :param _context: context
+    :type _context: invoke.Context
+    :param config_path: path to configuration file
+    :type config_path: str
+    """
+
+    import tensorflow as tf
+    import vlogging
+
+    import net.data
+    import net.logging
+    import net.ml
+    import net.processing
+    import net.utilities
+
+    config = net.utilities.read_yaml(config_path)
+
+    logger = net.utilities.get_logger(
+        path=config["log_path"]
+    )
+
+    training_data_loader = net.data.Cars196DataLoader(
+        config=config,
+        dataset_mode=net.constants.DatasetMode.TRAINING
+    )
+
+    dataset = tf.data.Dataset.from_generator(
+        generator=lambda: iter(training_data_loader),
+        output_types=(tf.float32, tf.float32),
+        output_shapes=(tf.TensorShape([None, 224, 224, 3]), tf.TensorShape([None]))
+    )
+
+    for element in dataset.take(3):
+
+        images_tensor, labels_tensor = element
+
+        images = [net.processing.ImageProcessor.get_denormalized_image(image) for image in images_tensor.numpy()]
+
+        logger.info(
+            vlogging.VisualRecord(
+                title="images",
+                imgs=images,
+                footnotes=str(labels_tensor.numpy())
+            )
+        )
+
+
+@invoke.task
 def visualize_predictions(_context, config_path):
     """
     Visualize image similarity ranking predictions
@@ -79,7 +131,9 @@ def visualize_predictions(_context, config_path):
         dataset_mode=net.constants.DatasetMode.TRAINING
     )
 
-    similarity_computer = net.ml.ImagesSimilarityComputer()
+    similarity_computer = net.ml.ImagesSimilarityComputer(
+        image_size=config["image_size"]
+    )
 
     similarity_computer.model = tf.keras.models.load_model(
         filepath=config["model_dir"],
